1efe2827bbd9ca81e926c2eafb44f19dcca73c1d
AAMECH <52366626+AAMECH@users.noreply.github.com>
2019-10-20 15:54:50 -0500

    Organizacion de vistas
    vistas finalizadas de crud's

--- Prestamos/admin.py
+++ Prestamos/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Rol
+from .models import Categoria, Equipo,Facultad, Espacio, facultad_equipo, Estado, Reserva
 
-admin.site.register(Rol)
+admin.site.register(Categoria)
+admin.site.register(Equipo)
+admin.site.register(Facultad)
+admin.site.register(Espacio)
+admin.site.register(facultad_equipo)
+admin.site.register(Estado)
+admin.site.register(Reserva)
--- Prestamos/models.py
+++ Prestamos/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
 class Rol(models.Model):
     id_rol = models.PositiveIntegerField
     nombre = models.CharField(max_length=50)
 
     def __str__ (self):
         return "{0} ({1})".format(self.nombre, self.id_rol)
-
+        
 class Usuario(models.Model):
     cedula = models.CharField(max_length=12)
     nombre = models.CharField(max_length=50)
@@ -17,41 +18,70 @@ class Usuario(models.Model):
 
     def __str__ (self):
         return "({0}) {1}".format(self.cedula, self.nombre)
-    
+
 class Categoria(models.Model):
     id_categoria = models.PositiveIntegerField
     nombre = models.CharField(max_length=50)
 
     def __str__ (self):
-        return "{0} ({1})".format(self.nombre, self.id_categoria)
+        return "{0}".format(self.nombre)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
 
 class Equipo(models.Model):
     id_equipo = models.CharField(max_length=12)
     nombre_equipo = models.CharField(max_length=50)
     id_categoria= models.ForeignKey(Categoria, null=False, blank=False, on_delete=models.CASCADE)
+    
 
     def __str__ (self):
         return "{0}-{1} ({2})".format(self.nombre_equipo, self.id_equipo, self.id_categoria)
 
 class Facultad(models.Model):
-    id_facultad = models.CharField(max_length=12)
+    id_facultad = models.PositiveIntegerField
     nombre_facultad = models.CharField(max_length=50)
 
     def __str__ (self):
-        return "{0}-{1}".format(self.nombre_facultad, self.id_facultad)
+        return "{0}".format(self.nombre_facultad)
 
 class Espacio(models.Model):
-    id_espacio = models.CharField(max_length=12)
+    id_espacio = models.PositiveIntegerField
     nombre_espacio = models.CharField(max_length=50)
-    id_facultad= models.ForeignKey(Categoria, null=False, blank=False, on_delete=models.CASCADE)
+    id_facultad= models.ForeignKey(Facultad, null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__ (self):
-        return "{0}-{1} ({2})".format(self.id_espacio, self.nombre_espacio, self.id_facultad)
+        return "{0} ({1})".format(self.nombre_espacio, self.id_facultad)
 
 class facultad_equipo(models.Model):
     id_facultad= models.ForeignKey(Facultad, null=False, blank=False, on_delete=models.CASCADE)
     id_equipo= models.ForeignKey(Equipo, null=False, blank=False, on_delete=models.CASCADE)
     id_facultad_equipo = models.PositiveIntegerField
+    fecha_ingresado = models.DateField("Fecha De Creacion", auto_now= True, auto_now_add=False)
 
     def __str__(self):
-        return "{0} - {1} ({2})".format(self.id_facultad, self.id_equipo, self.id_facultad_equipo)
+        return "{0} - {1}".format(self.id_facultad, self.id_equipo)
+
+class Estado(models.Model):
+    id_estado = models.PositiveIntegerField
+    nombre_estado = models.CharField(max_length=50)
+
+    def __str__ (self):
+        return "{0}".format(self.nombre_estado)
+
+class Reserva(models.Model):
+    id_reserva = models.PositiveIntegerField
+    id_espacio= models.ForeignKey(Espacio, null=False, blank=False, on_delete=models.CASCADE)
+    id_estado= models.ForeignKey(Estado, null=False, blank=False, on_delete=models.CASCADE)
+    id_equipo= models.ForeignKey(facultad_equipo, null=False, blank=False, on_delete=models.CASCADE)
+    detalle_reserva = models.CharField(max_length=400)
+    fecha_ingresado = models.DateField("Fecha De Creacion", auto_now= True, auto_now_add=False)
+
+    def __str__ (self):
+        return "{0}".format(self.detalle_reserva)
+
+class Comentarios(models.Model):
+    id_comentario = models.PositiveIntegerField
+    detalle_reserva = models.CharField(max_length=400)
+
