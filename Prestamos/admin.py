4976d6f74f62bc5b90bf7bfed783376f3b85887d
edinsongaan <31551947+edinsongaan@users.noreply.github.com>
2019-11-18 03:05:32 -0500

    administracion completa
    Creacion completa del modulo de administracion, falta profesor y personal

--- Prestamos/admin.py
+++ Prestamos/admin.py
@@ -1,12 +1,61 @@
 from django.contrib import admin
-from .models import Categoria, Equipo,Facultad, Espacio, facultad_equipo, Estado, Reserva
+from .models import Categoria, Equipo,Facultad, Espacio, facultad_equipo, Reserva, Comentarios, comentario_servicio
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = (
+        'fecha_solicitud',
+        'id_equipo',
+        'id_espacio',
+        'detalle_reserva',
+    )
+    list_filter = (
+        'fecha_solicitud',
+    )
+    search_fields = (
+        'id_equipo',
+        'id_espacio',
+    )
+
+class ComentariosAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_reserva',
+        'id_estado',
+        'detalle_reserva',
+    )
+
+    list_filter = (
+        'id_estado',
+    )
+
+class comentario_servicioAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_estado',
+        'detalle_servicio',
+    )
+    list_filter = (
+        'id_estado',
+    )
+
+
+class facultad_equipoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_facultad',
+        'id_equipo',
+        'fecha_ingresado',
+    )
+    list_filter = (
+        'fecha_ingresado',
+        'id_facultad',
+    )
+    search_fields = (
+        'id_equipo',
+    )
 
 admin.site.register(Categoria)
 admin.site.register(Equipo)
 admin.site.register(Facultad)
 admin.site.register(Espacio)
-admin.site.register(facultad_equipo)
-admin.site.register(Estado)
-admin.site.register(Reserva)
-
-#inventando un merge
+admin.site.register(facultad_equipo, facultad_equipoAdmin)
+admin.site.register(Reserva, ReservaAdmin)
+admin.site.register(Comentarios, ComentariosAdmin)
+admin.site.register(comentario_servicio, comentario_servicioAdmin)
--- Prestamos/models.py
+++ Prestamos/models.py
@@ -2,25 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Rol(models.Model):
-    id_rol = models.PositiveIntegerField
-    nombre = models.CharField(max_length=50)
-
-    def __str__ (self):
-        return "{0} ({1})".format(self.nombre, self.id_rol)
-        
-class Usuario(models.Model):
-    cedula = models.CharField(max_length=12)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    telefono = models.CharField(max_length=10)
-    id_rol= models.ForeignKey(Rol, null=False, blank=False, on_delete=models.CASCADE)
-
-    def __str__ (self):
-        return "({0}) {1}".format(self.cedula, self.nombre)
-
 class Categoria(models.Model):
-    id_categoria = models.PositiveIntegerField
     nombre = models.CharField(max_length=50)
 
     def __str__ (self):
@@ -31,7 +13,7 @@ class Categoria(models.Model):
         verbose_name_plural = "Categorias"
 
 class Equipo(models.Model):
-    id_equipo = models.CharField(max_length=12)
+    id_equipo = models.CharField(max_length=12, primary_key=True)
     nombre_equipo = models.CharField(max_length=50)
     id_categoria= models.ForeignKey(Categoria, null=False, blank=False, on_delete=models.CASCADE)
     
@@ -40,14 +22,12 @@ class Equipo(models.Model):
         return "{0}-{1} ({2})".format(self.nombre_equipo, self.id_equipo, self.id_categoria)
 
 class Facultad(models.Model):
-    id_facultad = models.PositiveIntegerField
     nombre_facultad = models.CharField(max_length=50)
 
     def __str__ (self):
         return "{0}".format(self.nombre_facultad)
 
 class Espacio(models.Model):
-    id_espacio = models.PositiveIntegerField
     nombre_espacio = models.CharField(max_length=50)
     id_facultad= models.ForeignKey(Facultad, null=False, blank=False, on_delete=models.CASCADE)
 
@@ -57,31 +37,47 @@ class Espacio(models.Model):
 class facultad_equipo(models.Model):
     id_facultad= models.ForeignKey(Facultad, null=False, blank=False, on_delete=models.CASCADE)
     id_equipo= models.ForeignKey(Equipo, null=False, blank=False, on_delete=models.CASCADE)
-    id_facultad_equipo = models.PositiveIntegerField
     fecha_ingresado = models.DateField("Fecha De Creacion", auto_now= True, auto_now_add=False)
 
     def __str__(self):
         return "{0} - {1}".format(self.id_facultad, self.id_equipo)
 
-class Estado(models.Model):
-    id_estado = models.PositiveIntegerField
-    nombre_estado = models.CharField(max_length=50)
-
-    def __str__ (self):
-        return "{0}".format(self.nombre_estado)
 
 class Reserva(models.Model):
-    id_reserva = models.PositiveIntegerField
     id_espacio= models.ForeignKey(Espacio, null=False, blank=False, on_delete=models.CASCADE)
-    id_estado= models.ForeignKey(Estado, null=False, blank=False, on_delete=models.CASCADE)
     id_equipo= models.ForeignKey(facultad_equipo, null=False, blank=False, on_delete=models.CASCADE)
-    detalle_reserva = models.CharField(max_length=400)
-    fecha_ingresado = models.DateField("Fecha De Creacion", auto_now= True, auto_now_add=False)
+    detalle_reserva = models.TextField()
+    fecha_solicitud = models.DateTimeField("Fecha De Solicitud", auto_now_add=False)
 
     def __str__ (self):
         return "{0}".format(self.detalle_reserva)
 
 class Comentarios(models.Model):
-    id_comentario = models.PositiveIntegerField
-    detalle_reserva = models.CharField(max_length=400)
+    
+    id_reserva= models.ForeignKey(Reserva, null=False, blank=False, on_delete=models.CASCADE)
+    detalle_reserva = models.TextField()
+    
+    LOAN_STATUS = (
+        ('r', 'Reservado'),
+        ('e', 'Entregado'),
+        ('f', 'Finalizado'),
+    )
+    id_estado= models.CharField(max_length=1, choices=LOAN_STATUS, null=False, default='r')
 
+    def __str__ (self):
+        return "{0}".format(self.id_reserva)
+
+class comentario_servicio(models.Model):
+    detalle_servicio = models.TextField()
+    
+    LOAN_STATUS = (
+        ('p', 'Pesimo'),
+        ('m', 'Malo'),
+        ('r', 'Regular'),
+        ('b', 'Bueno'),
+        ('e', 'Excelente'),
+    )
+    id_estado= models.CharField(max_length=1, choices=LOAN_STATUS, null=False)
+
+    def __str__ (self):
+        return "{0}".format(self.id_estado)
